29d18d78a93f6b4c294db1575837bdaeb7aa870e
tinpham <heoconlinhat123@gmail.com>
2023-11-25 19:53:57 +0700

    CONFIG-1: Remove some constant

--- src/common/constant/api.constant.ts
+++ src/common/constant/api.constant.ts
@@ -1,15 +1,1 @@
-export const API = {
-  AUTH: {
-    TAGS: 'Auth',
-    CONTROLLER: 'auth',
-    LOGIN: 'login',
-  },
-  USER: {
-    TAGS: 'User',
-    CONTROLLER: 'user',
-    STORE: {
-      ROUTE: 'store',
-      OPERATION: 'Store user',
-    },
-  },
-};
+export const API = {};
--- src/common/constant/database.constant.ts
+++ src/common/constant/database.constant.ts
@@ -1,16 +1,1 @@
-export const DATABASE_TABLE = {
-  USERS: {
-    NAME: 'users',
-    SCHEMA: {
-      ID: 'id',
-      USERNAME: 'username',
-      PASSWORD: 'password',
-      DISPLAY_NAME: 'display_name',
-      PHONE: 'phone',
-      EMAIL: 'email',
-      CREATED_AT: 'created_at',
-      UPDATED_AT: 'updated_at',
-      DELETED_AT: 'deleted_at',
-    },
-  },
-};
+export const DATABASE_TABLE = {};
